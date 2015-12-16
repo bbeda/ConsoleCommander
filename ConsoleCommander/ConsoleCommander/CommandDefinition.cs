65e468ce227e97a26d633692d87b3e7c021916f6
bbeda <bogdan.beda@hotmail.com>
2015-12-16 09:21:54 +0000

    - separated commander classes into a separate assembly

--- ConsoleCommander/ConsoleCommander/CommandDefinition.cs
+++ ConsoleCommander/ConsoleCommander/CommandDefinition.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleCommander.App
+namespace ConsoleCommander
 {
     public class CommandDefinition
     {
